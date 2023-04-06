8bb5e009eef11d4fa949ca996a0263f2b76fe634
Zhipicommunity <129773176+Zhipicommunity@users.noreply.github.com>
2023-04-07 00:17:34 +0800

    Update models.py

--- wxcloudrun/models.py
+++ wxcloudrun/models.py
@@ -15,3 +15,27 @@ class Counters(models.Model):
 
     class Meta:
         db_table = 'Counters'  # 数据库表名
+        
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        db_table='User'
+
+class Pet(models.Model):
+    name = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    age = models.IntegerField()
+    breed = models.CharField(max_length=100)
+    description = models.TextField()
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+    class Meta:
+        db_table='PET'
+    
